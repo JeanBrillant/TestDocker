7d1784e7162d5f59f1e83b57a120e80d4ddf5d30
JeanBrillant <ramahatombojeanbrillant@gmail.com>
2025-06-10 15:56:20 +0300

    Mise a jour reussite

--- script.js
+++ script.js
@@ -2,26 +2,27 @@ import {Message} from "./models/Message.js"
 
 let form = document.forms['formulaire']
 
-const save = (e) => {
+const save = async (e) => {
     e.preventDefault()
     let data = new FormData(e.currentTarget)
     let object = {}
-    data.forEach(async (v,k) => {
+    data.forEach((v,k) => {
         if(v.trim() == "" || k.trim() == ""){
             alert('Tsy mety')
             return
         }
         else{
             object[k] = v
-            try {
-                await Message.insert(object);
-                console.log('Message inséré avec succès');
-            } catch (error) {
-                console.log('Erreur dans script')
-                console.error('Erreur lors de l\'insertion:', error);
-            }
         }
     })
+
+    try {
+        await Message.insert(object);
+        console.log('Message inséré avec succès');
+    } catch (error) {
+        console.log('Erreur dans script')
+        console.error('Erreur lors de l\'insertion:', error);
+    }
 }
 
 // // Recuperation des donnees
